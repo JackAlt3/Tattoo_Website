2e2a376131e5e039cb41d6abacbdc4c10a8b9572
JasonP11 <pereirajason2004@gmail.com>
2025-06-09 22:00:51 +0530

    (.)(.)

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ import './Navbar.css'; // Import the CSS file
 function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/home" className="nav-link active">HOME</Link>
+      <Link to="/" className="nav-link active">HOME</Link>
       <Link to="/gallery" className="nav-link">GALLERY</Link>
       <Link to="/offer" className="nav-link">OFFER</Link>
       <Link to="/booking" className="nav-link">BOOKING</Link>
