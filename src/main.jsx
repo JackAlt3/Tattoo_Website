bdf5f6486c8f7256f7b64a3074482233a114a18b
JasonP11 <pereirajason2004@gmail.com>
2025-06-10 15:14:49 +0530

    g

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import App from './App.jsx'
 import Gallery from './gallery.jsx';
+import Contact from './contact.jsx';
 import Booking from './booking.jsx';
 import Appoint from './Appontform.jsx';
 import './index.css'
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/" element={<App />} />
           <Route path="/about" element={<Appoint />} />
           <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
           {/* <Route path="/home" element={<HOME />} /> */}
           <Route path="/booking" element={<Booking/>} />
         </Routes>
