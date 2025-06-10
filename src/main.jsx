0327bce2175e3220ac9f1ccc81fbcfa20389b921
Brevon2004 <brevonnunes@gmail.com>
2025-06-10 15:19:58 +0530

    diddy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import App from './App.jsx'
 import Gallery from './gallery.jsx';
+import contact from './contact.jsx';
 import Booking from './booking.jsx';
 import Appoint from './Appontform.jsx';
 import './index.css'
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/gallery" element={<Gallery />} />
           {/* <Route path="/home" element={<HOME />} /> */}
           <Route path="/booking" element={<Booking/>} />
+          <Route path="/contact" element={<contact />} />
         </Routes>
       </Router>
   </StrictMode>
